5122cd38766f2c65ff62cc97677a37809779f85c
Rohit <36408613+rohit1636@users.noreply.github.com>
2019-10-06 17:11:30 +0530

    implemented bfs algorithm in python3.

---
+++ Python/bfs.py
@@ -0,0 +1,29 @@
+from collections import defaultdict 
+class Graph: 
+	def __init__(self): 
+
+		self.graph = defaultdict(list) 
+
+	def addEdge(self,u,v): 
+		self.graph[u].append(v) 
+ 
+	def BFS(self, s): 
+
+		visited = [False] * (len(self.graph)) 
+		queue = [] 
+		queue.append(s) 
+		visited[s] = True
+
+		while queue: 
+			s = queue.pop(0) 
+			print (s, end = " ") 
+
+			for i in self.graph[s]: 
+				if visited[i] == False: 
+					queue.append(i) 
+					visited[i] = True
+
+g = Graph() 
+# add the edges using addEdge method in the above class.
+print ("BFS from the given vertex is") 
+g.BFS(vertex) 
